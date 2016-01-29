0b5001409d2b69649d02796a25dc93464a76f2ab
Christian R <gorax94@web.de>
2016-01-29 17:09:21 +0100

    Update main.js

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -16,46 +16,94 @@ angular.module('sbAdminApp')
 	$scope.currentProject = {
 		
 	};
-	
-	$scope.alreadyInit = false;
 
 	var init = function(){
-		//funzt nicht, da App komplett neustartet?!
-		if($scope.alreadyInit){}
-		//Initialisieren des ersten Charts (Commits..)
-		else{
-			projectProvider.getProjects().then(function(data){
-				$scope.projects = data.data;
-			}); 
-			$scope.alreadyInit = true;
-			// $scope.currentProject.projectName = document.getElementById('repoHeader').getText();
-			$scope.stargazers($scope.getCurrentUrl());
-		}
+		projectProvider.getProjects().then(function(data){
+			$scope.projects = data.data;
+		}); 
+		$scope.alreadyInit = true;
+		$scope.currentProject.projectName = 'openui5';
+		document.getElementById('Selector').addEventListener("change", $scope.changeMeasure);
+		$scope.currentProject.page = 28;
+		$scope.getStats($scope.getCurrentUrl());
 	};
 
 	$scope.getCurrentUrl = function(){
 		var url = "https://api.github.com/repos/SAP/" + $scope.currentProject.projectName;
+		$scope.currentProject.years = new Array(0);
+		$scope.currentProject.stars = new Array(0);
 		switch(document.getElementById('Selector').selectedIndex){
-			case 0: url += "/stats/commit_activity"; break;
-			case 1: url += "/stargazers?per_page=100&?page=0"; break;
-			case 2: url += "/forks"; break;
-			case 3: url += "/releases"; break;
-			case 4: url += "/contributors"; break;
-			case 5: url += "/pulls"; break;
-			case 6: url += "/issues"; break;
+			case 0: url += "/stats/commit_activity"; $scope.currentProject.measure = "commit_activity"; break;
+			case 1: url += ("/stargazers?per_page=100?&page=" + $scope.currentProject.page); $scope.currentProject.measure = "stargazers"; break;
+			case 2: url += "/forks"; $scope.currentProject.measure = "forks"; break;
+			case 3: url += "/releases"; $scope.currentProject.measure = "releases"; break;
+			case 4: url += "/contributors"; $scope.currentProject.measure = "contributors"; break;
+			case 5: url += "/pulls"; $scope.currentProject.measure = "pulls"; break;
+			case 6: url += "/issues"; $scope.currentProject.measure = "issues"; break;
 		}
 		return url;
 	};
 
 	$scope.changeMeasure = function(){
-
+		var data = $scope.getStats($scope.getCurrentUrl());
+		
 	};
 
-	$scope.stargazers = function(urlLink){
-	  	var result = [], i = 0, resultX = null;
-		function returnResult(result1){
-			result = result1;
-			$scope.currentProject.commits = result;
+	$scope.changeChart = function(measure){
+		console.log("Hello" + measure);
+	};
+
+	$scope.getStats = function(urlLink){
+	  	var result = [], i = 0;
+		function returnResult(result){
+			var data = [], labels = [];
+			// $scope.currentProject.data = result;
+			switch($scope.currentProject.measure){
+				case "commit_activity":
+					for(var i = 0; i < result.length; i++){
+						var date = new Date("" + result[i].week);
+						var month = date.getMonth();
+						var year = date.getYear();
+						
+					}
+					break;
+				case "stargazers":
+					for(var i = 0; i < result.length; i++){
+						var date = new Date(result[i].starred_at);
+						var year = date.getYear() + 1900;
+						for(var j = 0; j <= $scope.currentProject.years.length; j++){
+							if(year === $scope.currentProject.years[j]){
+								$scope.currentProject.stars[j] += 1;
+								break;
+							}
+							else{
+								if(j === $scope.currentProject.years.length){
+									$scope.currentProject.years.push(year);
+									$scope.currentProject.stars[$scope.currentProject.years.length-1] = 1;
+									break;
+								}
+							}
+						}
+					}
+					if(result !== []){
+						// $scope.currentProject.page++;
+						// $scope.getStats($scope.getCurrentUrl());
+					}
+					else{
+						break;
+					}
+					break;
+				case "forks":
+					break;
+				case "releases":
+					break;
+				case "contributors":
+					break;
+				case "pulls":
+					break;
+				case "issues":
+					break;
+			}
 		};
 
 		$.ajax({
